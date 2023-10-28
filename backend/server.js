44ca7dc624c8ad940540ebdcff3411e486d34410
copy-ninja1 <emmajnr1000@gmail.com>
2023-10-28 04:50:58 +0100

    chore: add .env to server /use 1 package installer

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const { getSubtitles, getVideoDetails } = require('youtube-caption-extractor');
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const app = express();
-const port = 5000; // Change this to your desired port number
+const port = process.env.PORT || 5000; 
 
 app.get('/api/captions', async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'chrome-extension://jdhabhfaijjpgmikaeagicopigokojhi');
+  res.setHeader('Access-Control-Allow-Origin', process.env.CHROME_EXT);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  // next();
+
   const { videoID, lang } = req.query;
 
   try {
     const subtitles = await getSubtitles({ videoID, lang });
     const videoDetails = await getVideoDetails({ videoID, lang });
+
     res.status(200).json({ subtitles, videoDetails });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message || "An Error Occured in the Server" });
   }
 });
 
